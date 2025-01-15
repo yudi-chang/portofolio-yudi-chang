26d84c375cb136c3fcb5ba2e30123b8959b03ef5
Yudi Chang <m.yudi.chang@gmail.com>
2025-01-15 08:21:47 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
     siteName: "Yudi Chang Portfolio",
     images: [
         {
-            url: "/preview.png",
+            url: "https://yudi-chang-portofolio.netlify.app/preview.png",
             width: 1200,
             height: 630,
             alt: "A preview of Yudi Chang's portfolio",
@@ -32,7 +32,7 @@ twitter: {
     card: "summary_large_image",
     title: "Yudi Chang",
     description: "A showcase of my work and skills.",
-    images: ["/preview.png"],
+    images: ["https://yudi-chang-portofolio.netlify.app/preview.png"],
 },
 };
 
